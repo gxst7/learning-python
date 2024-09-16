8bd05ea512452193f8c04af9f7cf64e9dede4785
alexandergostilo <alexandergostilo@gmail.com>
2024-09-16 19:44:22 +0300

    add module circle and testing file

---
+++ test_import.py
@@ -0,0 +1,9 @@
+import circle as c
+
+pi = 3
+print('pi here:', pi)
+print('pi', c.pi)
+print('area:', c.area(3))
+print('circumference:', c.circumference(3))
+print('surface:', c.sphere_surface(3))
+print('volume:', c.sphere_volume(3))
